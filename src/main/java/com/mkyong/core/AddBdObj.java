e9b4419cebb4ece3ff19965a6759141f2840959b
shedimon <dshedimon@gmail.com>
2015-12-22 17:30:10 +0200

    Добавление + удаление

---
+++ src/main/java/com/mkyong/core/AddBdObj.java
@@ -0,0 +1,41 @@
+package com.mkyong.core;
+
+import java.net.UnknownHostException;
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.Mongo;
+import com.mongodb.MongoException;
+
+/**
+ * Created by shedimon on 22.12.2015.
+ */
+public class AddBdObj {
+    public static void main(String[] args) {
+
+        try {
+
+            Mongo mongo = new Mongo("localhost", 27017);
+            DB db = mongo.getDB("pizza");
+
+            String[] s1 ={"name","type","price","taste"};
+            String[] s2={"Trapparoni","Huizza","1488","awful"};
+
+            DBCollection collection = db.getCollection("product");
+            addNewDoc(s1,s2,collection);
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (MongoException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void addNewDoc(String[] field, String[] args, DBCollection c){
+
+        BasicDBObject document = new BasicDBObject();
+        for (int i=1; i<field.length; i++) {
+            document.put(field[i], args[i]);
+        }
+        c.insert(document);
+    }
+}
